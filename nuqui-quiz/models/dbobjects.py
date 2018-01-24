483ce16c53b721f31228f9f254576332bdc75643
Zoyela <Tanja.kuery@students.unibe.ch>
2018-01-24 16:35:58 +0100

    Create db tables (user, score, meal, food) and methods (create and remove user)

---
+++ nuqui-quiz/models/dbobjects.py
@@ -0,0 +1,61 @@
+from sqlalchemy import Column, Integer, String, Table, ForeignKey
+from sqlalchemy.orm import relationship
+import timestamp as timestamp
+
+
+class Question(Base):
+    __tablename__ = 'downloaded_questions'
+
+    id = Column(Integer, primary_key=True)
+    answer = Column(String)
+    question = Column(String)
+    value = Column(Integer)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True),
+    name = Column(String),
+    score = Column(Integer, ForeignKey('scores.id'))
+
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "points": self.score.points,
+            "last_points": self.score.latest_points
+        }
+
+
+class Score(Base):
+    __tablename__ = 'scores'
+
+    id = Column(Integer, primary_key=True),
+    points = Column(Integer),
+    latest_points = Column(Integer),
+
+
+class Meal(Base):
+    __tablename__='meals'
+
+    id = Column(Integer, primary_key=True),
+    timestamp = Column(timestamp),
+    calories = Column(Integer),
+    food = Column(String)
+
+    def to_dictionary(self):
+        return {
+            "id": self.id,
+            "timestamp": self.timestamp,
+            "calories": self.calories,
+            "food": self.food
+        }
+
+
+
+
+
+
+
+
--- nuqui-quiz/nuqui-quiz.py
+++ nuqui-quiz/nuqui-quiz.py
@@ -1,6 +1,27 @@
 # a quiz app that tooks the question from http://www.jservice.io/ and put it into the db with https://github.com/nuqui-chatbot/nuqui-question-database
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from modules.dbobjects import User
+from modules.dbobjects import Score
+
+engine = create_engine('sqlite://questions.db')
+Session = sessionmaker(bind=engine)
 
 
-engine = create_engine('sqlite://questions.de')
+def create_user(user_id, user_name):
+    session = Session()
+    user_score = Score(points=0, latest_points=0)
+    user = User(id=user_id, name=user_name, score=user_score)
+    session.add(user_score)
+    session.add(user)
+    session.commit()
+
+
+def remove_user(user_id):
+    session = Session()
+    user = session.query(User).filter_by(id=id).one()
+    session.delete(user)
+    session.commit()
+
+
 
