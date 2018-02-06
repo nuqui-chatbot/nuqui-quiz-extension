f885c57b4af6d1b390fefc2bb24880c7ce3467a7
Lars Vögtlin <lvoegtlin@gmail.com>
2018-02-06 12:16:05 +0100

    adapted the setup file

--- setup.py
+++ setup.py
@@ -83,11 +83,10 @@ setup(
         'Development Status :: 3 - Alpha',
 
         # Indicate who your project is intended for
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
+        'Intended Audience :: Developers', 
 
         # Pick your license as you wish
-        'License :: OSI Approved :: GNU License',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both. 
